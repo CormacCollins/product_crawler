3462817d7b7d55a1c0f1a632d9326cb02766c7f0
Cormac <cormac.collins.ep@gmail.com>
2020-03-18 19:59:30 +1100

    Table creator work

--- Source/task_threader.py
+++ Source/task_threader.py
@@ -6,7 +6,7 @@ import threading
 
 thr_count = multiprocessing.cpu_count()
 
-class myThread(threading.Thread):
+class Crawl_thread(threading.Thread):
     def __init__(self, i, url, info_list, crawler):
         threading.Thread.__init__(self)
         self.num = i
@@ -27,21 +27,17 @@ def thread_tasks(links, info_list, crawler, batch_count):
     print('Running batch {}'.format(batch_count))
     # run multiple threds at once in line with the number of cores on machine
     for i in range(0, thr_count):
-        #print("Getting product from url {}".format(l))
-
         if links:
             l = links.pop(0)
-            thrd = myThread(i, l, info_list, crawler)
+            thrd = Crawl_thread(i, l, info_list, crawler)
             thrd.start()
         else:
             pass
 
     
     while threading.active_count() > 1:
-        
-        #print(threading.active_count())
-        #keep running
-        #print()
+        #Runs until extra threads are finished
         pass
+    
     print('Batch finished {}'.format(batch_count))
         
