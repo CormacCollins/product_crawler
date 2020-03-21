1fbddad62f370935b8525c7a056e853f99d7130f
Ryan Ashton <ryan.mark.ashton@gmail.com>
2020-03-21 11:06:54 +1100

    updates

--- Source/nutricia_crawler.py
+++ Source/nutricia_crawler.py
@@ -12,44 +12,56 @@ from time import sleep
 # TO DO - Create a proper object out of the code below
 
 
-chromedriver = "../Drivers/chromedriver.exe"
-
-driver =  webdriver.Chrome(chromedriver)
-
-driver.set_page_load_timeout(150)
-
-url = "https://www.nutriciahcp.com/adult/products/#"
-
-driver.get(url)
-
-# Wait until page loads
-driver.implicitly_wait(30)
-# Click "I am a HCP"
-driver.find_element_by_xpath("//*[@id='HCPModal']/div/div/div/div/a[1]").click()
-driver.implicitly_wait(5)
-# Click "I am from Mainland UK" - to get the most amount of products
-driver.find_element_by_xpath("//*[@id='ctl00_head_CountrySwitcher_notLoggedInUK']").click()
-
-sleep(10)
-# Click view all to get all the products on the one page
-view_all = driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_ViewAllButton']")
-view_all.click()
-
-sleep(30)
-
-# Now that the whole page is showing in the chromedriver, we can scrape the whole thing
-soup = BeautifulSoup(driver.page_source, "html.parser")
-
-mylist = []
 
 
-soup_pro_list = soup.find(class_='products-list')
 
-# Get all the a tags - links to products
+class Link_loader_nutricia():
 
-for link in soup_pro_list.find_all('a'):
-    mylist.append("https://www.nutriciahcp.com"+link.get('href'))
+    chromedriver = "../Drivers/chromedriver.exe"
 
-df = pd.DataFrame(mylist)
+    driver =  webdriver.Chrome(chromedriver)
 
-df.to_csv('../Data/Nutricia/nutricia_links.csv')
+    driver.set_page_load_timeout(150)
+
+
+    url = ["https://www.nutriciahcp.com/adult/products/#",
+     "https://www.nutriciahcp.com/adult/products/paediatrics/#",
+     "https://www.nutriciahcp.com/adult/products/metabolics/#"]
+
+
+        def get_product_links_nutricia(url)
+
+            mylist = []
+
+            for u in url:
+                # Initiate driver with url
+                driver.get(url)
+                # Wait until page loads
+                driver.implicitly_wait(30)
+                # Click "I am a HCP"
+                driver.find_element_by_xpath("//*[@id='HCPModal']/div/div/div/div/a[1]").click()
+                driver.implicitly_wait(5)
+                # Click "I am from Mainland UK" - to get the most amount of products
+                driver.find_element_by_xpath("//*[@id='ctl00_head_CountrySwitcher_notLoggedInUK']").click()
+                # Add sleep to allow for slow internet connections
+                sleep(10)
+                # Click view all to get all the products on the one page
+                view_all = driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_ViewAllButton']")
+                view_all.click()
+                # Allow javascript on page to load "view all"
+                sleep(30)
+                # Now that the whole page is showing in the chromedriver, we can scrape the whole thing
+                soup = BeautifulSoup(driver.page_source, "html.parser")
+                # Get the products
+                soup_pro_list = soup.find(class_='products-list')
+                # Get all the a tags - links to products
+                    for link in soup_pro_list.find_all('a'):
+                        mylist.append("https://www.nutriciahcp.com"+link.get('href'))
+
+
+            df = pd.DataFrame(mylist)
+
+            df.to_csv('../Data/Nutricia/nutricia_links.csv')
+
+
+
